37c028d44403ec26a51bc3a83f4f891ed866dd50
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-26 21:18:48 +0700

    feat(site layout): change px-4 xl:px-0 to header , main and footer

--- src/layouts/site-layout.tsx
+++ src/layouts/site-layout.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const SiteLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="flex flex-col h-screen overflow-hidden px-4 xl:px-0">
-      <header className="bg-green-700 py-5 shrink-0">header</header>
-      <main className="grow overflow-hidden my-5">{children}</main>
-      <footer className="bg-green-300 py-1 shrink-0">footer</footer>
+    <div className="flex flex-col h-screen overflow-hidden">
+      <header className="bg-green-700 py-5 shrink-0 px-4 xl:px-0">
+        header
+      </header>
+      <main className="grow overflow-hidden my-5 px-4 xl:px-0">{children}</main>
+      <footer className="bg-green-300 py-1 shrink-0 px-4 xl:px-0">
+        footer
+      </footer>
     </div>
   );
 };
