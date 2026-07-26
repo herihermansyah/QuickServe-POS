057b20c9e58123905d1a94a86b008053efd4ea00
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-26 21:15:16 +0700

    feat(site layout): add px-4 xl:px-0

--- src/layouts/site-layout.tsx
+++ src/layouts/site-layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SiteLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
+    <div className="flex flex-col h-screen overflow-hidden px-4 xl:px-0">
       <header className="bg-green-700 py-5 shrink-0">header</header>
       <main className="grow overflow-hidden my-5">{children}</main>
       <footer className="bg-green-300 py-1 shrink-0">footer</footer>
