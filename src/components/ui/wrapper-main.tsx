cc69d3acaeaa1b119b5122aa31fd208a6902eb5a
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-26 21:14:06 +0700

    feat(wrapper main): add wrapper main

--- src/components/ui/wrapper-main.tsx
+++ src/components/ui/wrapper-main.tsx
@@ -1,3 +1,4 @@
+import {cn} from "@/lib/cn";
 import React, {forwardRef} from "react";
 
 interface WrapperMainProps {
@@ -8,7 +9,11 @@ interface WrapperMainProps {
 const WrapperMain = forwardRef<HTMLDivElement, WrapperMainProps>(
   ({className, children, ...props}, ref) => {
     return (
-      <div ref={ref} {...props} className={className}>
+      <div
+        ref={ref}
+        {...props}
+        className={cn("container mx-auto xl:w-main h-full", className)}
+      >
         {children}
       </div>
     );
