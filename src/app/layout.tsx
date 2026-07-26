4e60044b11e6ae9f72128c0e2f2d800dea70003d
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-26 21:02:24 +0700

    feat(layout): add site layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Poppins} from "next/font/google";
 import "./globals.css";
+import SiteLayout from "@/layouts/site-layout";
 
 const poppinsFonts = Poppins({
   subsets: ["latin"],
@@ -71,7 +72,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${poppinsFonts.className} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="h-full flex flex-col bg-orange-50">
+        <SiteLayout>{children}</SiteLayout>
+      </body>
     </html>
   );
 }
