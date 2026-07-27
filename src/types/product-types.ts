920d658e597090b6b7e41290ba0d31e94e14a8e2
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-27 08:08:10 +0700

    feat(product type): create product type

---
+++ src/types/product-types.ts
@@ -0,0 +1,45 @@
+export interface Dimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface Review {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface ProductMeta {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+}
+
+export interface ProductTypes {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand?: string;
+  sku: string;
+  weight: number;
+  dimensions: Dimensions;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: "In Stock" | "Low Stock" | "Out of Stock" | string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: ProductMeta;
+  images: string[];
+  thumbnail: string;
+}
