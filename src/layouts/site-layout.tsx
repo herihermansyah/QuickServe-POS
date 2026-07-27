81786fd64459e1c71df286eb8ed6044f4af2f66e
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-27 22:13:40 +0700

    style(site layout): change green 700 to 900 and add shadow dropdown

--- src/layouts/site-layout.tsx
+++ src/layouts/site-layout.tsx
@@ -4,7 +4,7 @@ import Header from "./header";
 const SiteLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      <header className="bg-green-700 shrink-0 py-3 px-4 xl:px-0">
+      <header className="bg-green-900 shrink-0 py-3 px-4 xl:px-0 shadow-dropdown">
         <Header />
       </header>
       <main className="grow overflow-y-auto my-5 px-4 xl:px-0">{children}</main>
