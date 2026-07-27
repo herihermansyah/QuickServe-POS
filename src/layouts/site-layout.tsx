8df6acf2b6ffa66dc144adeec1c049fd24ca7ddc
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-27 19:32:52 +0700

    feat(site layout): change overflow y auto on main

--- src/layouts/site-layout.tsx
+++ src/layouts/site-layout.tsx
@@ -7,7 +7,7 @@ const SiteLayout = ({children}: {children: React.ReactNode}) => {
       <header className="bg-green-700 shrink-0 py-3 px-4 xl:px-0">
         <Header />
       </header>
-      <main className="grow overflow-hidden my-5 px-4 xl:px-0">{children}</main>
+      <main className="grow overflow-y-auto my-5 px-4 xl:px-0">{children}</main>
     </div>
   );
 };
