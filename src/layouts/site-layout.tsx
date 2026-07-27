ba131b88e5031895f3360e3129eccc579c99e8e0
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-27 08:23:31 +0700

    feat(site layout): deleted footer, add header

--- src/layouts/site-layout.tsx
+++ src/layouts/site-layout.tsx
@@ -1,15 +1,13 @@
 import React from "react";
+import Header from "./header";
 
 const SiteLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      <header className="bg-green-700 py-5 shrink-0 px-4 xl:px-0">
-        header
+      <header className="bg-green-700 shrink-0 py-3 px-4 xl:px-0">
+        <Header />
       </header>
       <main className="grow overflow-hidden my-5 px-4 xl:px-0">{children}</main>
-      <footer className="bg-green-300 py-1 shrink-0 px-4 xl:px-0">
-        footer
-      </footer>
     </div>
   );
 };
