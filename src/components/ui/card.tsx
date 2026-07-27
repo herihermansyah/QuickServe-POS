9d3292ead806fe4d0ec9b49ee4fcdad54cea04eb
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-27 20:02:00 +0700

    feat(card): implementing a reusable card ui

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,76 @@
+import {cn} from "@/lib/cn";
+import React, {forwardRef} from "react";
+
+interface CardProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({className, children, ...props}, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className={cn(
+          "flex flex-col gap-3 rounded-md p-2 shadow-dropdown",
+          className,
+        )}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+Card.displayName = "Card";
+
+const CardHeader = forwardRef<HTMLDivElement, CardProps>(
+  ({className, children, ...props}, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className={cn("flex flex-col gap-0.5", className)}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+CardHeader.displayName = "CardHeader";
+
+const CardTitle = forwardRef<HTMLHeadingElement, CardProps>(
+  ({className, children, ...props}, ref) => {
+    return (
+      <h3
+        ref={ref}
+        {...props}
+        className={cn("truncate font-semibold", className)}
+      >
+        {children}
+      </h3>
+    );
+  },
+);
+
+CardTitle.displayName = "CardTitle";
+
+const CardFooter = forwardRef<HTMLDivElement, CardProps>(
+  ({className, children, ...props}, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className={cn("flex items-center justify-between", className)}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+CardFooter.displayName = "CardFooter";
+
+export {Card, CardHeader, CardTitle, CardFooter};
