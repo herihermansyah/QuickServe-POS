6933eafa61fec0e092960c13e87345b79c43c29a
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-30 16:43:52 +0700

    feat(page): add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {Suspense} from "react";
 import OrderPage from "@/components/order-page";
 
 const page = () => {
-  return <OrderPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrderPage />
+    </Suspense>
+  );
 };
-
 export default page;
