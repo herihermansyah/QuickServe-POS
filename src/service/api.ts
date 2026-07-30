a1ad69aba228269549c5d6c9d7ec075a8fdae530
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-30 11:36:36 +0700

    feat(base api url): add or dummy api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const apiURL = process.env.NEXT_PUBLIC_API_URL;
+const apiURL = process.env.NEXT_PUBLIC_API_URL || "https://dummyjson.com";
 
 export const api = axios.create({
-  baseURL: apiURL,
+  baseURL: apiURL.endsWith("/") ? apiURL.slice(0, -1) : apiURL,
   headers: {
     "Content-Type": "application/json",
   },
