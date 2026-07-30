017b602700f8f625620b901a1eab31674340cec8
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-30 11:37:40 +0700

    feat(search bar header): add handle search to search bar

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,14 +1,31 @@
+"use client";
 import Logo from "@/components/logo";
 import SearchBar from "@/components/search-bar";
 import WrapperMain from "@/components/ui/wrapper-main";
+import {useRouter, useSearchParams} from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+
+  const handleSearch = (value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    params.delete("category");
+    router.replace(`?${params.toString()}`);
+  };
+
   return (
     <WrapperMain className="flex items-center justify-between gap-10">
       <Logo />
       <div className="w-full md:w-1/2">
-        <SearchBar />
+        <SearchBar value={search} onChange={handleSearch} />
       </div>
     </WrapperMain>
   );
