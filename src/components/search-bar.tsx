903a7b7d8329d823aadd85c80adab46a22beeb2b
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-30 11:38:26 +0700

    feat(search bar): add type and props value, onchange to search bar

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -3,12 +3,16 @@ import React from "react";
 
 interface SearchBarProps {
   className?: string;
+  value?: string;
+  onChange: (value: string) => void;
 }
 
-const SearchBar = ({className}: SearchBarProps) => {
+const SearchBar = ({className, value, onChange}: SearchBarProps) => {
   return (
     <input
       type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
       placeholder="Search Product . . . . . . . . ."
       className={cn(
         "outline-none bg-amber-100 rounded-md py-4 px-4 w-full text-gray-500",
