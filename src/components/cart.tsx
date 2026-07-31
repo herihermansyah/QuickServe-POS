a94d3b7b98c8168cb7ab68738ec44f69c0972da5
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-08-01 01:38:38 +0700

    faat(cart): complete cart , and calculate summery cart

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -6,13 +6,9 @@ import Button from "./ui/button";
 import {cn} from "@/lib/cn";
 import {IoIosArrowUp} from "react-icons/io";
 import {AnimatePresence, motion} from "motion/react";
-
-const cartData = Array.from({length: 15}).map((_, i) => ({
-  id: 1 + i,
-  name: "nama product yang sangat panjang",
-  qty: 5,
-  price: "$5000",
-}));
+import {useCartStore} from "@/store/use-cart-store";
+import {formatCurrency} from "@/utils/format";
+import Loading from "./loading";
 
 interface CartProps {
   className?: string;
@@ -20,76 +16,125 @@ interface CartProps {
 
 const CartContent = forwardRef<HTMLDivElement, CartProps>(
   ({className}, ref) => {
+    const cartItems = useCartStore((state) => state.cartItems);
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [isMounted, setIsMounted] = React.useState(false);
+
+    const incrementQuantity = useCartStore((state) => state.incrementQuantity);
+    const decrementQuantity = useCartStore((state) => state.decrementQuantity);
+    const removeFromCart = useCartStore((state) => state.romoveFromCart);
+    const getSummeryCart = useCartStore((state) => state.getSummeryCart);
+
+    const {subTotal, totalDiscount, taxAmount, grandTotal} = getSummeryCart();
+
+    React.useEffect(() => {
+      setTimeout(() => setIsLoading(false), 1000);
+    }, []);
+
+    React.useEffect(() => {
+      setTimeout(() => {
+        setIsMounted(true);
+      }, 1000);
+    }, []);
+
     return (
       <Card
         ref={ref}
         className={cn(
           "h-full bg-white flex-1 min-h-0 overflow-hidden p-0",
-          "print:visible print:absolute print:inset-0 print:h-auto print:overflow-visible",
-          "print:shadow-none print:px-50",
+          "print:visible print:absolute print:inset-0 print:overflow-visible",
+          "print:shadow-none print:px-50 print:h-fit",
           className,
         )}
       >
         <CardHeader className="pt-5">
           <CardTitle className="text-center">ORDER LIST</CardTitle>
           <div className="grid grid-cols-[1fr_90px_50px] items-center bg-amber-100 px-4 py-1 text-sm font-medium">
-            <span className="text-left">name</span>
-            <span className="text-center">qty</span>
-            <span className="text-right">price</span>
+            <span className="text-left">NAME</span>
+            <span className="text-center">QTY</span>
+            <span className="text-right">PRICE</span>
           </div>
         </CardHeader>
         <CardContent className=" h-full flex-1 overflow-y-auto print:overflow-visible">
           {/* Item Row Grid */}
-          <div className="flex flex-col gap-2">
-            {cartData.map((item) => (
-              <div
-                key={item.id}
-                className="grid grid-cols-[1fr_58px_60px] print:grid-cols-[1fr_95px_60px] items-center px-3 py-2 print:py-0 text-sm"
-              >
-                {/* Section Nama + Trash Icon */}
-                <div className="flex items-center gap-1.5 min-w-0">
-                  <button className="shrink-0 text-gray-500 hover:text-red-500 cursor-pointer transition-colors print:hidden">
-                    <CiTrash size={18} />
-                  </button>
-                  {/* Truncate nama produk */}
-                  <h3 className="truncate font-medium pr-3 print:pl-1">
-                    {item.name}
-                  </h3>
-                </div>
+          {isLoading ? (
+            <div className="h-full overflow-hidden">
+              <Loading />
+            </div>
+          ) : (
+            <div className="flex flex-col gap-2">
+              {cartItems.map((item) => (
+                <div
+                  key={item.product.id}
+                  className="grid grid-cols-[1fr_70px_60px] print:grid-cols-[1fr_95px_60px] items-center px-3 py-2 print:py-0 text-sm"
+                >
+                  {/* Section Nama + Trash Icon */}
+                  <div className="flex items-center gap-1.5 min-w-0">
+                    <button
+                      onClick={() => removeFromCart(item.product.id)}
+                      className="shrink-0 text-gray-500 hover:text-red-500 cursor-pointer transition-colors print:hidden"
+                    >
+                      <CiTrash size={18} />
+                    </button>
+                    {/* Truncate nama produk */}
+                    <h3 className="truncate font-medium pr-3 print:pl-1">
+                      {item.product.title}
+                    </h3>
+                  </div>
 
-                {/* Qty (Center) */}
-                <div className="flex items-center gap-3">
-                  <Button variant={"outline"} className="p-0.5 print:hidden">
-                    +
-                  </Button>
-                  <span className="font-bold text-[18px] border-b print:border-none border-green-700 flex items-center gap-10">
-                    <span className="hidden print:block font-normal">x</span>
-                    <span>{item.qty}</span>
+                  {/* Qty (Center) */}
+                  <div className="flex items-center gap-3 relative z-5">
+                    <Button
+                      onClick={() => decrementQuantity(item.product.id)}
+                      variant={"outline"}
+                      className="p-0.5 print:hidden"
+                    >
+                      -
+                    </Button>
+                    <span className="font-bold text-[18px] border-b print:border-none border-green-700 flex items-center gap-10">
+                      <span className="hidden print:block font-normal">x</span>
+                      <span>{item.quantity}</span>
+                    </span>
+                    <Button
+                      onClick={() => incrementQuantity(item.product.id)}
+                      variant={"outline"}
+                      className="p-0.5 print:hidden"
+                    >
+                      +
+                    </Button>
+                  </div>
+
+                  {/* Price (Right) */}
+                  <span className="text-right font-semibold">
+                    {formatCurrency(item.totalPrice)}
                   </span>
-                  <Button variant={"outline"} className="p-0.5 print:hidden">
-                    -
-                  </Button>
                 </div>
-
-                {/* Price (Right) */}
-                <span className="text-right font-semibold">{item.price}</span>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          )}
         </CardContent>
         <CardFooter className="p-4 border-t print:mt-5 border-gray-300">
           <div className="flex flex-col gap-1 font-medium capitalize">
-            <span>sub total: $100000</span>
-            <span>discount: $10000</span>
             <span>
-              tax :<span className="font-bold">11 %</span>
+              sub total :{" "}
+              {isMounted ? formatCurrency(subTotal) : formatCurrency(0)}
             </span>
             <span>
-              total amount :
-              <span className="font-bold underline underline-offset-4">
-                $10000000,000
+              discount :{" "}
+              {isMounted ? formatCurrency(totalDiscount) : formatCurrency(0)}
+            </span>
+            <div className="flex items-center gap-2">
+              <span>tax 11 % :</span>
+              <span>
+                {isMounted ? formatCurrency(taxAmount) : formatCurrency(0)}
               </span>
-            </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <span>total amount :</span>
+              <span className="font-bold underline underline-offset-4">
+                {isMounted ? formatCurrency(grandTotal) : formatCurrency(0)}
+              </span>
+            </div>
           </div>
         </CardFooter>
       </Card>
@@ -101,11 +146,14 @@ CartContent.displayName = "CartContent";
 
 const Cart = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const emptyCart = useCartStore((state) => state.clearCart);
+  const cartItems = useCartStore((state) => state.cartItems);
   const handlePrint = () => {
     return window.print();
   };
+
   return (
-    <div className="h-full overflow-hidden flex flex-col gap-2 md:gap-5 print:p-0">
+    <div className="h-full overflow-hidden flex flex-col gap-2 print:p-0">
       {/* background while pop up cart */}
       {isOpen && (
         <div className="bg-black/80 fixed inset-0 block lg:hidden print:hidden" />
@@ -145,13 +193,25 @@ const Cart = () => {
         >
           <IoIosArrowUp size={20} />
         </motion.button>
-        <Button
-          onClick={handlePrint}
-          size={"md"}
-          className="justify-center relative z-15"
+        <div
+          className={cn("grid grid-cols-[1fr_105px] gap-2 whitespace-nowrap")}
         >
-          Print / Download
-        </Button>
+          <Button
+            onClick={handlePrint}
+            size={"md"}
+            className="justify-center relative z-15"
+          >
+            Print / Download
+          </Button>
+          {cartItems.length > 0 && (
+            <Button
+              onClick={emptyCart}
+              className="justify-center relative z-15 bg-red-500"
+            >
+              Empty Cart
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
