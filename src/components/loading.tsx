2300cc17758762c87f26c8bc6fb075968e12f09f
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-08-01 01:37:17 +0700

    style(loading): add animate spin

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,7 +3,7 @@ import {AiOutlineLoading3Quarters} from "react-icons/ai";
 
 const Loading = () => {
   return (
-    <div className="h-full flex items-center justify-center text-gray-300">
+    <div className="h-full flex items-center justify-center text-gray-300 animate-spin">
       <AiOutlineLoading3Quarters size={40} />
     </div>
   );
